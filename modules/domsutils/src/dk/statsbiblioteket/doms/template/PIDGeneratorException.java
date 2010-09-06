7d9760672adcc335d56b730ff3fdf249bce32080
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-09-06 12:07:21 +0000

    Cleaned the unused modules away

--- modules/domsutils/src/dk/statsbiblioteket/doms/template/PIDGeneratorException.java
+++
@@ -1,7 +0,0 @@
-package dk.statsbiblioteket.doms.template;
-
-public class PIDGeneratorException extends RuntimeException {
-    public PIDGeneratorException(String message, Exception cause) {
-        super(message, cause);
-    }
-}
