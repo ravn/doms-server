6aa47590f8945c633edda2ad1b644b0e75cbdc78
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2010-10-25 13:15:43 +0000

    Caches for doms central

--- modules/centralWebservice/centralWebserviceImpl/src/dk/statsbiblioteket/doms/central/connectors/fedora/Fedora.java
+++ modules/centralWebservice/centralWebserviceImpl/src/dk/statsbiblioteket/doms/central/connectors/fedora/Fedora.java
@@ -25,245 +25,61 @@
  * under the License.
  */
 
-
 package dk.statsbiblioteket.doms.central.connectors.fedora;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
-import com.sun.jersey.api.client.UniformInterfaceException;
-import com.sun.jersey.api.client.WebResource;
+import dk.statsbiblioteket.doms.central.connectors.BackendMethodFailedException;
 import dk.statsbiblioteket.doms.central.connectors.BackendInvalidCredsException;
 import dk.statsbiblioteket.doms.central.connectors.BackendInvalidResourceException;
-import dk.statsbiblioteket.doms.central.connectors.BackendMethodFailedException;
-import dk.statsbiblioteket.doms.central.connectors.Connector;
-import dk.statsbiblioteket.doms.webservices.Credentials;
 
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
  * User: abr
- * Date: Aug 25, 2010
- * Time: 1:50:31 PM
+ * Date: Oct 25, 2010
+ * Time: 11:51:52 AM
  * To change this template use File | Settings | File Templates.
  */
-public class Fedora extends Connector {
-    private static Client client = Client.create();
-    public static final String STATE_ACTIVE = "A";
-    public static final String STATE_INACTIVE = "I";
-    public static final String STATE_DELETED = "D";
-    private WebResource restApi;
+public interface Fedora {
+    String STATE_ACTIVE = "A";
+    String STATE_INACTIVE = "I";
+    String STATE_DELETED = "D";
 
-
-
-
-
-    public Fedora(Credentials creds, String location)
-            throws MalformedURLException {
-        super(creds, location);
-        restApi = client.resource(location + "/objects/");
-
-    }
-
-    public void modifyObjectState(String pid, String state)
+    void modifyObjectState(String pid, String state)
             throws
             BackendMethodFailedException,
             BackendInvalidCredsException,
-            BackendInvalidResourceException {
-        try {
-            restApi.path(URLEncoder.encode(pid, "UTF-8"))
-                    .queryParam("state", state)
-                    .header("Authorization", credsAsBase64())
-                    .put();
-        } catch (UnsupportedEncodingException e) {
-            throw new BackendMethodFailedException("UTF-8 not known....", e);
-        } catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 
-    public void modifyDatastreamByValue(String pid,
-                                        String datastream,
-                                        String contents)
+    void modifyDatastreamByValue(String pid,
+                                 String datastream,
+                                 String contents)
             throws
             BackendMethodFailedException,
             BackendInvalidCredsException,
-            BackendInvalidResourceException {
-        try {
-            restApi.path(URLEncoder.encode(pid, "UTF-8"))
-                    .path("/datastreams/")
-                    .path(URLEncoder.encode(datastream,"UTF-8"))
-                    .queryParam("mimeType","text/xml")
-                    .header("Authorization", credsAsBase64())
-                    .post(contents);
-        } catch (UnsupportedEncodingException e) {
-            throw new BackendMethodFailedException("UTF-8 not known....", e);
-        } catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 
-    public String getXMLDatastreamContents(String pid, String datastream)
+    String getXMLDatastreamContents(String pid, String datastream)
             throws
             BackendMethodFailedException,
             BackendInvalidCredsException,
-            BackendInvalidResourceException {
-        try {
-            String contents = restApi.path(URLEncoder.encode(pid, "UTF-8"))
-                    .path("/datastreams/")
-                    .path(URLEncoder.encode(datastream, "UTF-8"))
-                    .path("/content")
-                    .header("Authorization", credsAsBase64())
-                    .get(String.class);
-            return contents;
-        } catch (UnsupportedEncodingException e) {
-            throw new BackendMethodFailedException("UTF-8 not known....", e);
-        } catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 
-    public void addRelation(String pid, String subject, String property, String object)
+    void addRelation(String pid, String subject, String property, String object)
             throws
             BackendMethodFailedException,
             BackendInvalidCredsException,
-            BackendInvalidResourceException {
-        try {
-            if (!subject.startsWith("info:fedora/")){
-                subject = "info:fedora/"+subject;
-            }
-            if (!object.startsWith("info:fedora/")){
-                object = "info:fedora/"+object;
-            }
-            restApi.path(URLEncoder.encode(pid, "UTF-8"))
-                    .path("/relationships/new")
-                    .queryParam("subject",subject)
-                    .queryParam("predicate",property)
-                    .queryParam("object",object)
-                    .header("Authorization", credsAsBase64())
-                    .post();
-        } catch (UnsupportedEncodingException e) {
-            throw new BackendMethodFailedException("UTF-8 not known....", e);
-        } catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 
-    public List<String> listObjectsWithThisLabel(String label)
+    List<String> listObjectsWithThisLabel(String label)
             throws
             BackendInvalidCredsException,
             BackendMethodFailedException,
-            BackendInvalidResourceException {
-        //TODO sanitize label
-        try {
-            String query = "select $object\n"
-                           + "from <#ri>\n"
-                           + "where $object <fedora-model:label> '"+label+"'";
-            String objects = client.resource(location)
-                    .path("/risearch")
-                    .queryParam("type", "tuples")
-                    .queryParam("lang", "iTQL")
-                    .queryParam("format", "CSV")
-                    .queryParam("flush","true")
-                    .queryParam("stream","on")
-                    .queryParam("query", query)
-                    .header("Authorization", credsAsBase64())
-                    .post(String.class);
-            String[] lines = objects.split("\n");
-            List<String> foundobjects = new ArrayList<String>();
-            for (String line : lines) {
-                if (line.startsWith("\"")){
-                    continue;
-                }
-                if (line.startsWith("info:fedora/")){
-                    line = line.substring("info:fedora/".length());
-                }
-                foundobjects.add(line);
-            }
-            return foundobjects;
-        }  catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 
-    public void modifyObjectLabel(String pid, String name)
+    void modifyObjectLabel(String pid, String name)
             throws
             BackendMethodFailedException,
             BackendInvalidCredsException,
-            BackendInvalidResourceException {
-        try {
-            restApi.path(URLEncoder.encode(pid, "UTF-8"))
-                    .queryParam("label", name)
-                    .header("Authorization", credsAsBase64())
-                    .put();
-        } catch (UnsupportedEncodingException e) {
-            throw new BackendMethodFailedException("UTF-8 not known....", e);
-        } catch (UniformInterfaceException e) {
-            if (e.getResponse().getStatus()
-                == ClientResponse.Status.UNAUTHORIZED.getStatusCode()) {
-                throw new BackendInvalidCredsException(
-                        "Invalid Credentials Supplied",
-                        e);
-            } else if (e.getResponse().getStatus() == ClientResponse.Status.NOT_FOUND.getStatusCode()){
-                throw new BackendInvalidResourceException("Resource not found",e);
-            }
-            else  {
-                throw new BackendMethodFailedException("Server error", e);
-            }
-        }
-    }
+            BackendInvalidResourceException;
 }
