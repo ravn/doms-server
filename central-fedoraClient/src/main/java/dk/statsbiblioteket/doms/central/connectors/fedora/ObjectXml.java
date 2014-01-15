94737a1b56bc7293c670e9227db3c852e78fc032
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2014-01-15 12:16:04 +0100

    All the review comments from KFC. Mostly javadoc comments, more optimal xml header regexp

--- central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/ObjectXml.java
+++ central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/ObjectXml.java
@@ -16,6 +16,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+/**
+ * This class handles the translation between raw foxml and the cleaned version we want to emit.
+ */
 public class ObjectXml {
 
     private String pid;
@@ -23,6 +26,13 @@ public class ObjectXml {
     private final FedoraRest fedora;
     private final Long asOfTime;
 
+    /**
+     * Construct the ObjectXml from a raw dump, with a fedora client
+     * @param pid the pid of the object
+     * @param rawXml the raw dump
+     * @param fedora the fedora client
+     * @param asOfTime the cut of date
+     */
     public ObjectXml(String pid, String rawXml, FedoraRest fedora, Long asOfTime){
         this.pid = pid;
 
@@ -31,6 +41,14 @@ public class ObjectXml {
         this.asOfTime = asOfTime;
     }
 
+    /**
+     * Get the cleaned version of the xml
+     * @return the cleaned version
+     * @throws TransformerException
+     * @throws BackendInvalidResourceException
+     * @throws BackendMethodFailedException
+     * @throws BackendInvalidCredsException
+     */
     public String getCleaned() throws
                                TransformerException,
                                BackendInvalidResourceException,
@@ -43,18 +61,24 @@ public class ObjectXml {
 
         List<String> datastreamIDs = getManagedXmlDatastreams(xml);
 
-        StringBuilder temp = new StringBuilder(xml);
+        StringBuilder mutableXmlBlob = new StringBuilder(xml);
 
         for (String datastreamID : datastreamIDs) {
-            temp = inlineDatastream(temp, datastreamID, pid, asOfTime);
+            mutableXmlBlob = inlineDatastream(mutableXmlBlob, datastreamID, pid, asOfTime);
         }
 
 
-        return temp.toString();
+        return mutableXmlBlob.toString();
 
 
     }
 
+    /**
+     * Replace managed datastreams with a placeholderr
+     * @param xml the xml
+     * @return the xml with placeholders
+     * @throws TransformerException
+     */
     private String stubManagedDatastreams(String xml) throws TransformerException {
         Transformer transformer = XSLT.getLocalTransformer(
                 Thread.currentThread().getContextClassLoader().getResource("xslt/placeHoldersForManagedDatastreams.xslt"));
@@ -64,6 +88,12 @@ public class ObjectXml {
     }
 
 
+    /**
+     * Strip the hidden datastreams like AUDIT
+     * @param xml the xml string prestrip
+     * @return the xml blob without a trace of the hidden datastreams
+     * @throws TransformerException
+     */
     protected String stripHiddenDatastreams(String xml) throws TransformerException {
         Transformer transformer = XSLT.getLocalTransformer(
                 Thread.currentThread().getContextClassLoader().getResource("xslt/stripHiddenDatastreams.xslt"));
@@ -73,6 +103,17 @@ public class ObjectXml {
     }
 
 
+    /**
+     * Inline the given managed datastream. Will fetch the contents and insert it instead of the reference to the content.
+     * @param xml the xml blob
+     * @param dsid the datastream id
+     * @param pid the pid of the object
+     * @param asOfTime the cutoff date
+     * @return A stringbuilder with the datastream inlined
+     * @throws BackendInvalidResourceException
+     * @throws BackendInvalidCredsException
+     * @throws BackendMethodFailedException
+     */
     protected StringBuilder inlineDatastream(StringBuilder xml, String dsid, String pid, Long asOfTime) throws
                                                                                                         BackendInvalidResourceException,
                                                                                                         BackendInvalidCredsException,
@@ -80,13 +121,19 @@ public class ObjectXml {
 
 
         String realContent = fedora.getXMLDatastreamContents(pid, dsid, asOfTime);
-        String cleanedContent = realContent.replaceAll("\\<\\?xml(.+?)\\?\\>", "").trim();
+        String cleanedContent = realContent.replaceFirst("\\A\\<\\?xml(.+?)\\?\\>", "").trim();
 
         String str = "PLACEHOLDER_PREFIX" + dsid + "PLACEHOLDER_POSTFIX";
         int placeholderIndex = xml.indexOf(str);
         return xml.replace(placeholderIndex, placeholderIndex + str.length(), cleanedContent);
     }
 
+    /**
+     * Get the list of DSIDs for managed datastreams with mimetype text/xml
+     * @param xml the xml blob
+     * @return the list of datastream IDs to be inlined
+     * @throws TransformerException
+     */
     protected List<String> getManagedXmlDatastreams(String xml) throws TransformerException {
         Transformer transformer = XSLT.getLocalTransformer(
                 Thread.currentThread().getContextClassLoader().getResource("xslt/getManagedDatastreams.xslt"));
@@ -102,6 +149,12 @@ public class ObjectXml {
         return realResult;
     }
 
+    /**
+     * Strip all but the newest version of versionable datastreams
+     * @param xml the xml blob
+     * @return the xml blob with only one version of each datastream
+     * @throws TransformerException
+     */
     protected String stripOldVersions(String xml) throws TransformerException {
         Transformer transformer = XSLT.getLocalTransformer(
                 Thread.currentThread().getContextClassLoader().getResource("xslt/stripOldVersions.xslt"));
@@ -110,6 +163,13 @@ public class ObjectXml {
         return result.toString();
     }
 
+    /**
+     * Strip all version of a datastream that are newer than the asOfTime. If asOfTime is null, no change
+     * @param xml the xml blob
+     * @param asOfTime the cutoff date
+     * @return the xml with any newer datastream versions removed
+     * @throws TransformerException
+     */
     protected String modifyForDate(String xml, Long asOfTime) throws TransformerException {
         if (asOfTime != null){
             Date date = new Date(asOfTime);
