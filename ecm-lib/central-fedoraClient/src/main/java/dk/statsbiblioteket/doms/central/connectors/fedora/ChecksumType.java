02d9c7d6133523be0b0c252cb520b96db2dba2e7
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-03-17 12:43:57 +0100

    removed ecm-lib

--- ecm-lib/central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/ChecksumType.java
+++
@@ -1,5 +0,0 @@
-package dk.statsbiblioteket.doms.central.connectors.fedora;
-
-public enum ChecksumType {
-    MD5
-}
--- ecm-lib/central-fedoraClient/src/main/java/dk/statsbiblioteket/doms/central/connectors/fedora/structures/ObjectType.java
+++
@@ -1,9 +0,0 @@
-package dk.statsbiblioteket.doms.central.connectors.fedora.structures;
-
-/**
- * Created by IntelliJ IDEA. User: abr Date: 9/14/11 Time: 11:52 AM To change this template use File | Settings | File
- * Templates.
- */
-public enum ObjectType {
-    DATA_OBJECT, CONTENT_MODEL, TEMPLATE, FILE, COLLECTION;
-}
